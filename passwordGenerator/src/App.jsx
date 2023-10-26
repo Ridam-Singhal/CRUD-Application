2689756234af01c6812cf0217d25ab628fdb1a31
Ridam-Singhal <manusinghal84@gmail.com>
2023-10-26 16:52:51 +0530

    New margin added

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
     passwordGenerator();
   }, [length, numberAllowed, charAllowed, passwordGenerator]);
   return (
-    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700">
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-80 text-orange-500 bg-gray-700">
       <h1 className="text-white text-center my-3">Password Generator</h1>
       <div className="flex shadow-sm rounded-lg overflow-hidden mb-4">
         <input
