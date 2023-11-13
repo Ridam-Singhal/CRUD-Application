bfd18eacaaca2eddec68342ad9bb225934b5fbcc
Ridam-Singhal <manusinghal84@gmail.com>
2023-11-13 19:13:21 +0530

    Added 24 June Special Class

---
+++ Backend/SpecialClass/src/24June.js
@@ -0,0 +1,77 @@
+import app from "./app.js";
+import mongoose from "mongoose";
+
+const port = 3000;
+
+// Database Connection
+/*
+Two Important Points of DB 
+
+1. Database connection may fail
+2. Database is in another continent
+*/
+
+// mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+/*
+// Async IIFE
+(async () => {
+  try {
+    // db conncetion
+    await mongoose.connect("dbstring");
+    console.log("DB connected successfully");
+
+    app.on("error", () => {
+      console.log("ERROR ", err);
+      throw err;
+    });
+
+    const onListen = () => {
+      console.log(`Listening on port ${port}`);
+    };
+
+    app.listen(port, onListen);
+  } catch (error) {
+    console.error("ERROR", error);
+    throw error;
+  }
+})();
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// db connection using Promise
+new Promise((resolve, reject) => {
+  mongoose.connection.on("connected", () => {
+    resolve();
+  });
+
+  mongoose.connection.on("error", (err) => {
+    reject(err);
+  });
+
+  mongoose.connect("dbString");
+})
+  .then(() => {
+    console.log("DB connection successful");
+
+    app.on("error", () => {
+      console.log("ERROR ", error);
+      throw error;
+    });
+
+    const onListen = () => {
+      console.log(`Listening on port ${port}`);
+    };
+
+    app.listen(port, onListen);
+  })
+  .catch((err) => {
+    console.log("ERROR ", err);
+    throw err;
+  });
+
+*/
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
